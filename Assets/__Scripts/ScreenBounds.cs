c990cf606dbe9b4a121755de2f7ad175614403b2
José Juan Gálvez <jjgalper@gmail.com>
2019-01-31 17:07:39 -0600

    Refactoring code and comments

--- Assets/__Scripts/ScreenBounds.cs
+++ Assets/__Scripts/ScreenBounds.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class ScreenBounds : MonoBehaviour {
 
+    //cuando el algun objeto de juego salga de la pantalla.
     private void OnTriggerExit(Collider other)
     {
+        //Si es la nave o una bala, el objeto tiene que aparecer del otro lado 
         if(other.gameObject.tag == "Player" || other.gameObject.tag == "Bullet")
         {
             Vector3 exitPos = other.transform.position;
